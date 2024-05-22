dc64184f4689b5925926152f2917946240142561
Colin Marvin <colinmar@umich.edu>
2024-05-22 15:44:22 +0200

    Changing file structure to allow for frontend

---
+++ ix-blog-app/frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      // <Homepage />
+      // <BlogsPage />
+      // <CategoriesPage />
+    </div>
+  );
+}
+
+export default App;
