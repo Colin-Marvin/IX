ab71bfc025bae7a4b9ec5a09ad4343c5d605a0bc
Colin Marvin <colinmar@umich.edu>
2024-05-22 17:35:43 +0200

    Added proto navbar

--- ix-blog-app/frontend/src/components/HomePage/index.jsx
+++ ix-blog-app/frontend/src/components/HomePage/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Heading from "../Heading";
-//import Navbar from "../Navbar"; ADD NAVBAR BEFORE CONTAINER DIV
+import Navbar from "../Navbar";
 import BlogGrid from "../BlogGrid";
 import Footer from "../Footer";
 import SubHeading from "../SubHeading";
@@ -15,6 +15,7 @@ const categories = data.categories;
 export default function HomePage() {
   return (
     <>
+      <Navbar />
       <div className="container">
         <Heading />
         <SubHeading subHeading={"Recent Blog Posts"} />
