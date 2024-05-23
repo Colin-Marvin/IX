540c5904f4e6a52879035f52ad5f4c3336719091
Colin Marvin <colinmar@umich.edu>
2024-05-23 11:55:19 +0200

    Update header and footer

--- ix-blog-app/frontend/src/App.js
+++ ix-blog-app/frontend/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Homepage from "./components/HomePage";
+import Categoriespage from "./components/CategoriesPage";
 
 function App() {
   return (
     <div className="App">
       <Homepage />
+      <Categoriespage />
     </div>
   );
 }
