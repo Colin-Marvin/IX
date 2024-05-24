756e9a66e9755983682df0af9cdd48828a501384
Colin Marvin <colinmar@umich.edu>
2024-05-24 12:29:26 +0200

    Fixed blog page as landing page

--- ix-blog-app/frontend/src/components/BlogsPage/index.jsx
+++ ix-blog-app/frontend/src/components/BlogsPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Heading from "../../components/Heading";
 import BlogList from "../../components/BlogList";
